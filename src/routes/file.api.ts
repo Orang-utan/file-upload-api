8fde12eaaf0ce5b3a3840669cab7686dde9e1e0c
Daniel Tian <dtian.95@gmail.com>
2020-12-04 12:23:23 -0500

    test release

--- src/routes/file.api.ts
+++ src/routes/file.api.ts
@@ -1,14 +1,15 @@
 import express from "express";
-import AWS from "aws-sdk";
+// import AWS from "aws-sdk";
 import multer from "multer";
 import errorHandler from "./error";
 import auth from "../middleware/auth";
 import { AWS_S3_BUCKET_NAME } from "../utils/config";
 
 const router = express.Router();
-const s3 = new AWS.S3();
+// const s3 = new AWS.S3();
 
 // config multer
+// in memory strategy below
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
@@ -26,12 +27,14 @@ router.post("/upload", auth, upload.single("file"), (req, res) => {
     Body: bufferStream,
   };
 
-  s3.upload(uploadParams, (err: Error, data: AWS.S3.ManagedUpload.SendData) => {
-    if (err) return errorHandler(res, "An error occured with AWS", "500");
-    return res
-      .status(200)
-      .json({ success: true, data: { fileUrl: data.Location } });
-  });
+  return res.status(200).json({ success: true });
+
+  // s3.upload(uploadParams, (err: Error, data: AWS.S3.ManagedUpload.SendData) => {
+  //   if (err) return errorHandler(res, "An error occured with AWS", "500");
+  //   return res
+  //     .status(200)
+  //     .json({ success: true, data: { fileUrl: data.Location } });
+  // });
 });
 
 /* delete one file */
@@ -42,26 +45,29 @@ router.delete("/", auth, (req, res) => {
     Bucket: AWS_S3_BUCKET_NAME,
     Key: filename,
   };
+  return res.status(200).json({ success: true });
 
-  s3.deleteObject(uploadParams, (err: AWS.AWSError) => {
-    if (err) return errorHandler(res, "An error occured with AWS", "500");
-    return res
-      .status(200)
-      .json({ success: true, message: "Object successfully deleted" });
-  });
+  // s3.deleteObject(uploadParams, (err: AWS.AWSError) => {
+  //   if (err) return errorHandler(res, "An error occured with AWS", "500");
+  //   return res
+  //     .status(200)
+  //     .json({ success: true, message: "Object successfully deleted" });
+  // });
 });
 
 /* get all object keys */
 router.get("/all", auth, (_, res) => {
   const params = { Bucket: AWS_S3_BUCKET_NAME };
-  s3.listObjects(
-    params,
-    (err: AWS.AWSError, data: AWS.S3.ListObjectsOutput) => {
-      if (err) return errorHandler(res, "An error occured with AWS", "500");
+  return res.status(200).json({ success: true });
 
-      return res.status(200).json({ success: true, data });
-    }
-  );
+  // s3.listObjects(
+  //   params,
+  //   (err: AWS.AWSError, data: AWS.S3.ListObjectsOutput) => {
+  //     if (err) return errorHandler(res, "An error occured with AWS", "500");
+
+  //     return res.status(200).json({ success: true, data });
+  //   }
+  // );
 });
 
 export default router;
